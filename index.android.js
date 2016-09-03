52c78895a14ac259da18485ee4831dd905f6a1c8
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-09-03 01:54:55 -0300

    Added socketio initial support

--- index.android.js
+++ index.android.js
@@ -6,48 +6,86 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    NativeModules
 } from 'react-native';
 
+import Socket from 'react-native-socketio';
+
+const SOCKET_IP_ADDRESS = '192.168.1.45';
+const SOCKET_PORT = 8989;
+
+let socket = new Socket(`http://${SOCKET_IP_ADDRESS}:${SOCKET_PORT}`, { path: '/socket' });
+
+socket.on('connect', () => {
+    console.log('Socket connected');
+
+    socket.emit('robotregister', { nickName: 'UmaUmaUmaE' });
+
+    socket.on('robotregister:success', (robot) => {
+        console.log('Robot', robot);
+
+        setInterval(function () {
+            socket.emit('robotstream', { token: robot.token, buffer: 'asdn1H820UDAS-buffer-LOKO' });
+        }, 2000);
+    });
+});
+
+socket.connect();
+
 class bmobileclient extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+    constructor(props) {
+        super(props);
+
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 != r2
+        });
+
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                'John', 'Joel', 'James', 'Jimmy', 'Jackson'
+            ])
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Bmate
+                </Text>
+                <Text style={styles.instructions}>
+                    Keeping people in touch
+                </Text>
+                <ListView dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Text>{ rowData }</Text>}
+                />
+            </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('bmobileclient', () => bmobileclient);
