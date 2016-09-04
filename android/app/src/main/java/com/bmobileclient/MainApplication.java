3c7329c6d532be9369f986cdb583915903e4478e
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-09-04 10:33:55 -0300

    Added socket.io support and native-camera initial support

--- android/app/src/main/java/com/bmobileclient/MainApplication.java
+++ android/app/src/main/java/com/bmobileclient/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.gcrabtree.rctsocketio.SocketIoPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             new SocketIoPackage()
       );
     }
--- index.android.js
+++ index.android.js
@@ -8,76 +8,15 @@ import {
     NativeModules
 } from 'react-native';
 
-import Socket from 'react-native-socketio';
-import CONFIG from './app/config';
-
-let socket = new Socket(`http://${CONFIG.SOCKET_IP_ADDRESS}:${CONFIG.SOCKET_PORT}`, { path: '/socket' });
-
-socket.on('connect', () => {
-    console.log('Socket connected');
-
-    socket.emit('robotregister', { nickName: 'UmaUmaUmaE' });
-
-    socket.on('robotregister:success', (robot) => {
-        console.log('Robot', robot);
-
-        setInterval(function () {
-            socket.emit('robotstream', { token: robot.token, buffer: 'asdn1H820UDAS-buffer-LOKO' });
-        }, 2000);
-    });
-});
-
-socket.connect();
+import CameraStage from './app/component/camera-stage';
 
 class bmobileclient extends Component {
-    constructor(props) {
-        super(props);
-
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 != r2
-        });
-
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John', 'Joel', 'James', 'Jimmy', 'Jackson'
-            ])
-        };
-    }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Bmate
-                </Text>
-                <Text style={styles.instructions}>
-                    Keeping people in touch
-                </Text>
-                <ListView dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{ rowData }</Text>}
-                />
-            </View>
-    );
-  }
+            <CameraStage />
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
 
 AppRegistry.registerComponent('bmobileclient', () => bmobileclient);
